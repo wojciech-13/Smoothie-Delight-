cf3f7443052f69883b3dafad798b39dd79eb931f
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-05-30 00:03:24 +0200

    added some js to 4th page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+const list = document.querySelector('.list');
+const btn = document.querySelector('.btn');
+const listItems = list.querySelectorAll('li');
 
+
+function handleClick() {
+    setTimeout(function(){
+        list.classList.remove('hidden');
+        list.classList.add('show');
+    },1000)
+}
+
+
+btn.addEventListener('click', handleClick);
