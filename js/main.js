014d7bf1cfeeda7889db2a15d97bfc6ca237714e
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-05-31 00:06:09 +0200

    Navbar functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 const list = document.querySelector('.list');
 const btn = document.querySelector('.btn');
-const listItems = list.querySelectorAll('li');
 
-
-function handleClick() {
+function showBenefits() {
     setTimeout(function(){
         list.classList.remove('hidden');
         list.classList.add('show');
     },1000)
 }
 
+btn.addEventListener('click', showBenefits);
 
-btn.addEventListener('click', handleClick);
